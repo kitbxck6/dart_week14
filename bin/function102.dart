ce9c9332f011e2f37cfa93f96009ecdfd0b62eb7
kitbxck6 <kittipong8426@gmail.com>
2022-08-15 11:17:22 +0700

    first commit

---
+++ bin/function102.dart
@@ -0,0 +1,10 @@
+void main() {
+  message("Mark","57/8","06512410");
+}
+
+void message(String name,String address,String phone){
+
+  print("your name is ");
+  print("your address is ");
+  print("your phone number is");
+}
